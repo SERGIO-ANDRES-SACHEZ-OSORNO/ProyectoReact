5fc97d331664d5c18a76ee60d20e4d9aa612badb
SERGIO-ANDRES-SACHEZ-OSORNO <124899111+SERGIO-ANDRES-SACHEZ-OSORNO@users.noreply.github.com>
2023-10-29 22:02:30 -0500

    inicio actividad
    inicio actividad

---
+++ mi_proyecto/src/componentes/Header.js
@@ -0,0 +1,24 @@
+
+
+// import styled from 'styled-components';
+import React from "react";
+import "./App.css";
+const Header =()=>{
+    return(<p>Hola mundo</p>
+      
+    )
+    
+}
+
+// const Container = styled.div`
+//     min-height:60px;
+//     position:fixed;
+//     display:flex;
+//     align-items:center;
+//     justy-content: space-between;
+//     padding:0 20px;
+//     top:0;
+//     left:0;
+//     right:0;
+// `;
+export default Header;
